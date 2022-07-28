288f96bb28ae4b1a56f62211c7a1c2cfc4363b6e
KiOtRa <45514670+KiOtRa@users.noreply.github.com>
2022-07-28 11:55:25 +0200

    Update boucle.py
    Uses sinusoidal signals that are shifted so as to create a "wave"
    
    This was test code only

--- boucle.py
+++ boucle.py
@@ -1,11 +1,11 @@
 import numpy as np
 from math import *
-from pyfirmata import Arduino, PWM
+from pyfirmata import ArduinoMega, PWM
 from time import sleep
 
 #Pyfirmata
-port = '/dev/cu.usbserial-1420'
-board = Arduino(port)
+port = '/dev/cu.usbmodem14101'
+board = ArduinoMega(port)
 pin1 = 9
 pin2 = 10
 pin3 = 11
@@ -13,13 +13,10 @@ sleep(0.5)
 board.digital[pin1].mode = PWM
 board.digital[pin2].mode = PWM
 board.digital[pin3].mode = PWM
-def sendPwm(pin,pwm): #float entre 0 et 1
+def sendPwm(pin,pwm): #float between 0 et 1
     board.digital[pin].write(pwm)
     sleep(0.005)
-    print("alors: {}".format(pwm))
-
-
-#    
+  
 def start():
     global pin1
     global pin2
@@ -30,7 +27,7 @@ def start():
         sendPwm(pin2,abs(sin(i*0.01-0.2)))
         sendPwm(pin3,abs(sin(i*0.01-0.4)))
         i = i+1
-        print("Pourcentage de l'intensité pour les pins: {},{},{}".format(round(abs(sin(i*0.01))*100),round(abs(sin(i*0.01-0.2))*100),round(abs(sin(i*0.01-0.4)*100))))
+        print("Pourcentage of intensity for each pin: {},{},{}".format(round(abs(sin(i*0.01))*100),round(abs(sin(i*0.01-0.2))*100),round(abs(sin(i*0.01-0.4)*100))))
         sleep(0.1)
 
 
